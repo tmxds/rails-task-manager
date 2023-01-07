32f7b8da6625a9cc2fe9420acaa13eca160e1b12
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-01-07 14:02:51 +0900

    all user stories work

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -32,7 +32,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
   def set_task
